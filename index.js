7749a6a9552d39339295367eebd96639d4914119
Md. Naoshad Hossain <naoshad@Mds-MacBook-Pro.local>
2023-11-09 15:31:06 +0600

    add get and post in bidrequest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -18,163 +18,291 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const userCollection = client.db('discover-jobs').collection('user');
-    const jobsCollection = client.db('discover-jobs').collection('jobs');
-    const bidjobsCollection = client.db('discover-jobs').collection('bidjobs');
+    const userCollection = client.db("discover-jobs").collection("user");
+    const jobsCollection = client.db("discover-jobs").collection("jobs");
+    const bidjobsCollection = client.db("discover-jobs").collection("bidjobs");
+ const bidrequestjobsCollection = client.db("discover-jobs").collection("requstbidjobs");
 
 
-    app.post('/api/saveuser', async (req, res) => {
+
+    app.post("/api/saveuser", async (req, res) => {
       try {
         const user = req.body;
         const result = await userCollection.insertOne(user);
         res.send(result);
       } catch (error) {
         console.error(error);
-        
       }
     });
 
-    app.get('/api/saveuser', async (req, res) => {
+    app.get("/api/saveuser", async (req, res) => {
       try {
-        const userEmail = req.query.userEmail; 
-        const cursor = userCollection.find({ email: userEmail }); 
+        const userEmail = req.query.userEmail;
+        const cursor = userCollection.find({ email: userEmail });
         const result = await cursor.toArray();
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching user-specific job listings.' });
+        res
+          .status(500)
+          .json({
+            error:
+              "An error occurred while fetching user-specific job listings.",
+          });
       }
     });
 
     // jobs
-    app.get('/api/jobs', async (req, res) => {
-      try {   
+    app.get("/api/jobs", async (req, res) => {
+      try {
         const cursor = jobsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching job listings.' });
+        res
+          .status(500)
+          .json({ error: "An error occurred while fetching job listings." });
       }
     });
-    app.get('/api/jobs/:id', async (req, res) => {
+    app.get("/api/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
     });
-    
-    app.get('/api/myjobs', async (req, res) => {
+
+    app.get("/api/myjobs", async (req, res) => {
       try {
-        const userEmail = req.query.userEmail; 
-        const cursor = jobsCollection.find({ email: userEmail }); 
+        const userEmail = req.query.userEmail;
+        const cursor = jobsCollection.find({ email: userEmail });
         const result = await cursor.toArray();
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching user-specific job listings.' });
+        res
+          .status(500)
+          .json({
+            error:
+              "An error occurred while fetching user-specific job listings.",
+          });
       }
     });
 
-    app.post('/api/jobs', async (req, res) => {
-      try {
-        const job = req.body;
-        const result = await jobsCollection.insertOne(job);
-        res.send(result)
-      } catch (error) {
-        console.error(error);
-        
-      }
-    });
+  app.post("/api/jobs", async (req, res) => {
+  try {
+    const job = req.body;
 
-    app.put('/api/jobs/:id', async (req, res) => {
+    // Store the user's email along with the job details
+    job.email = req.user.email; // Assuming you have user information in req.user
+
+    const result = await jobsCollection.insertOne(job);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+    app.put("/api/jobs/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const updatedJob = req.body;
         const filter = { _id: new ObjectId(id) };
-        const result = await jobsCollection.updateOne(filter, { $set: updatedJob });
+        const result = await jobsCollection.updateOne(filter, {
+          $set: updatedJob,
+        });
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while updating the job.' });
+        res
+          .status(500)
+          .json({ error: "An error occurred while updating the job." });
       }
     });
-    
-    
+
     // Delete a job
-    app.delete('/api/jobs/:id', async (req, res) => {
+    app.delete("/api/jobs/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const result = await jobsCollection.deleteOne(query);
-        res.send(result)
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while deleting the job.' });
-      }
-    });
-    
-
-    // bidjobs
-    app.get('/api/confirmJob', async (req, res) => {
-      
-      let query = { };
-      if(req.query?.email){
-        query = {email: req.query.email}
-      }
-      const result = await bidjobsCollection.findOne(query).toArray();
-      res.send(result);
-    });
-    
-
-    app.post('/api/confirmJob', async (req, res) => {
-      try {
-        const { jobId, userEmail, confirmationData, additionalInfo, phoneNumber, address, jobRelatedInfo } = req.body;
-    
-        const result = await bidjobsCollection.insertOne({
-          jobId,
-          userEmail,
-        
-          additionalInfo,
-          phoneNumber,
-          address,
-          jobRelatedInfo,
-          bidDate: new Date(),
-        });
-    
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while confirming the job.' });
+        res
+          .status(500)
+          .json({ error: "An error occurred while deleting the job." });
       }
     });
 
-    // cakbid
+    // bidjobs
+    app.get("/api/ConfirmedJobs", async (req, res) => {
+      const userEmail = req.query.email;
 
-  
+      if (!userEmail) {
+        return res.status(400).json({ error: "Email parameter is required" });
+      }
+
+      try {
+        const query = { email: userEmail };
+        const result = await bidjobsCollection.findOne(query);
+
+        if (!result) {
+          return res.status(404).json({ error: "No data found" });
+        }
+
+        res.json(result);
+      } catch (error) {
+        console.error(error);
+        res
+          .status(500)
+          .json({
+            error: "An error occurred while fetching confirmation data",
+          });
+      }
+    });
+
+   app.post("/api/getConfirmedJobs", async (req, res) => {
+  try {
+    const {
+      jobId, 
+      userEmail,
+      additionalInfo,
+      phoneNumber,
+      address,
+      jobRelatedInfo,
+    } = req.body;
+
+    const result = await bidjobsCollection.insertOne({
+      jobId, 
+      userEmail,
+      additionalInfo,
+      phoneNumber,
+      address,
+      jobRelatedInfo,
+      bidDate: new Date(),
+    });
+
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while confirming the job." });
+  }
+});
+
+    // getConfirmedJobs
+
+app.get("/api/getConfirmedJobs", async (req, res) => {
+  const userEmail = req.query.email;
+  const jobId = req.query.jobId;
+
+  if (!userEmail) {
+    return res.status(400).json({ error: "Email parameter is required" });
+  }
+
+  try {
+    let query;
+    if (jobId) {
+      
+      query = { userEmail, jobId };
+    } else {
+      query = { userEmail };
+    }
+
+    const result = await bidjobsCollection.find(query).toArray();
+
+    if (!result || result.length === 0) {
+      return res.status(404).json({ error: "No confirmed jobs found for the user" });
+    }
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching confirmed jobs" });
+  }
+});
+
+
+app.get("/api/bidRequests", async (req, res) => {
+  try {
+    const bidRequests = await bidrequestjobsCollection.find({ status: 'pending' }).toArray();
+    res.json(bidRequests);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching bid requests." });
+  }
+});
+app.get("/api/bidRequests/:id", async (req, res) => {
+  try {
+    const bidRequests = await bidrequestjobsCollection.find({ status: 'pending' }).toArray();
+    res.json(bidRequests);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching bid requests." });
+  }
+});
+
+// app.post("/api/bidRequests", async (req, res) => {
+//   try {
     
-    
-  
-// ...............
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+//      const {
+//       jobId, 
+//       userEmail,
+//       additionalInfo,
+//       phoneNumber,
+//       address,
+//       jobRelatedInfo,
+//     } = req.body;
+
+//        const result = await bidrequestjobsCollection.insertOne({
+//       jobId, 
+//       userEmail,
+//       additionalInfo,
+//       phoneNumber,
+//       address,
+//       jobRelatedInfo,
+//       bidDate: new Date(),
+//     });
+
+//     res.send(result)
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "An error occurred while submitting the bid request." });
+//   }
+// });
+
+   app.post('/api/bidRequests', async (req, res) => {
+            const bid = req.body;
+            
+            const result = await bidrequestjobsCollection.insertOne(bid);
+            res.send(result);
+        });
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('discover jobs server');
+app.get("/", (req, res) => {
+  res.send("discover jobs server");
 });
 
 app.listen(port, () => {
